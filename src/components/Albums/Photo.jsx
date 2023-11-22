52483f94af218143ddc72ab7d09b040a3a60b236
brachalevi <barlevi055@gmail.com>
2023-11-22 17:45:19 +0200

    creat alboms and link to the photo

--- src/components/Albums/Photo.jsx
+++ src/components/Albums/Photo.jsx
@@ -3,8 +3,13 @@ import React, { useEffect, useState } from "react";
 
 function Photo() {
   const [photo, setPhoto] = useState();
+
   const { id } = useParams();
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const getPhoto = async () => {
+      const data = fetch(``);
+    };
+  }, []);
   return <p>{id}</p>;
 }
 export default Photo;
