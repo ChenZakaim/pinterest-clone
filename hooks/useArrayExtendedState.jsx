8fe73b091421a0fc384d433f8c7db1f490fbb5b8
ChenZakaim <chenzakaim@hilma.tech>
2023-11-23 11:30:25 +0200

    added useArrayExtendedState

---
+++ hooks/useArrayExtendedState.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function useArrayExtendedState() {
+  const [stateArr, setStateArr] = useState([]);
+
+  function deleteFromStateArray(itemId) {
+    console.log("inside delItem");
+    const newArr = stateArr.filter((item) => item.id !== itemId);
+    console.log('newArr: ', newArr);
+    setStateArr(newArr);
+  }
+  function addItemToArr(item) {
+    console.log("inside addItem");
+    setStateArr((prev) => {
+      const newArr = [...prev];
+      newArr.push(item);
+      console.log("newArr: ", newArr);
+
+      return newArr;
+    });
+  }
+  function updateItem(id, newItem) {
+    console.log("inside updItem");
+    setStateArr((prev) => {
+      const newArr = [...prev];
+      let item = newArr.find((item) => item.id === id);
+      item = { ...item, ...newItem };
+      return newArr;
+    });
+  }
+  return [
+    stateArr,
+    setStateArr,
+    addItemToArr,
+    deleteFromStateArray,
+    updateItem,
+  ];
+}
+export default useArrayExtendedState;
